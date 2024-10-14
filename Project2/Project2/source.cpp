4b247ef6904df19eaf5b0f1d335352892e8b9f92
Filip1991 <filip.victor@yahoo.com>
2024-10-14 10:52:18 +0300

    Add files via upload

---
+++ Project2/Project2/source.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+//#include<string.h>
+using namespace std;
+
+class Student {
+	//atribute
+	string nume1;
+	char nume2[50];
+	char* nume3;
+	char serie;
+	int anStudiu;
+	int nrNote;
+	int* note;//vector alocat dinamic
+
+
+
+
+
+	//metode
+};
+
+
+
+
+int main() {
+
+	Student s;
+
+	return 0;
+
+}
+
